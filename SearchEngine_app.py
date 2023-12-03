9d7a675c3a464e235f248c31296979ecd65da678
asuchonska <91882538+asuchonska@users.noreply.github.com>
2023-12-03 21:05:15 +0100

    incorporating the crawler into the app - showing the real results

--- SearchEngine_app.py
+++ SearchEngine_app.py
@@ -1,23 +1,23 @@
 from flask import Flask, request, render_template
+from crawler import index, search 
 
 app = Flask(__name__)
 
 @app.route("/")
 def start():
-    #Instead of that:
-    #return "<form action='reversed' method='get'><input name='rev'></input></form>" 
     return render_template("start.html")
 
 @app.route("/search")
-def search():
+def search_results():
     if not 'rev' in request.args:
         return render_template("start.html")
     else:
     #getting the input
-    #tokenizing it
-    #pulling out the tokenized words from index
-    #printing out the results
         rev = request.args['rev']
-        a = [ rev, rev, rev]
-        return render_template('search.html', result = a, rev = rev)
+    #tokenizing it
+    
+    #pulling out the tokenized words from index
+        result = search (index, rev)
+    #printing out the results
+        return render_template('search.html', result = result, rev = rev)
         
