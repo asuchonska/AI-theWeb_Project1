6bbc949cb4fa7e0a2b270117395d6d2fdb63aa50
asuchonska <91882538+asuchonska@users.noreply.github.com>
2023-12-04 09:19:05 +0100

    correction of the errorhandler's position in SearchEngine.py

--- SearchEngine.py
+++ SearchEngine.py
@@ -2,11 +2,13 @@ from flask import Flask, request, render_template
 from crawler import index, search 
 
 import traceback
+
+
+app = Flask(__name__)
+
 @app.errorhandler(500)
 def internal_error(exception):
    return "<pre>"+traceback.format_exc()+"</pre>"
-
-app = Flask(__name__)
 
 @app.route("/")
 def start():
