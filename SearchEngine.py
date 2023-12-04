942e86b2d0c16d660a91c13550a7225bd4c97acb
asuchonska <91882538+asuchonska@users.noreply.github.com>
2023-12-04 08:01:34 +0100

    adding files necessary for upload to the server

--- SearchEngine.py
+++ SearchEngine.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, render_template
 from crawler import index, search 
 
+import traceback
+@app.errorhandler(500)
+def internal_error(exception):
+   return "<pre>"+traceback.format_exc()+"</pre>"
 
 app = Flask(__name__)
 
---
+++ SearchEngine.wsgi
@@ -0,0 +1,6 @@
+import sys
+
+sys.path.insert(1,'/home/user171/SearchEngine')
+
+# Import
+from SearchEngine import app as application
